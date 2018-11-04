80f16611001d69d17040b5a2748fba299d4843f9
Beverly Lau <beverly.lau@klarna.com>
2018-11-04 18:41:04 +0100

    Add views and assets

---
+++ spec/controllers/url_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'spec_helper'
+
+
+RSpec.describe UrlController, type: :controller do
+
+  describe 'show' do
+    context 'when valid short_url is given' do
+      it 'should redirect to the correct page' do
+      end
+    end
+  end
+end
--- spec/models/url_test.rb
+++ spec/models/url_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UrlControllerTest < ActionDispatch::IntegrationTest
+class UrlTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
